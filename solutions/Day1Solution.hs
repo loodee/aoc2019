96ab8d703f28081018d546a8cc7c24fcb6faa429
Thomas Li <guslithd@student.gu.se>
2019-12-01 13:04:36 +0100

    Day 1 part 2 solution

--- solutions/Day1Solution.hs
+++ solutions/Day1Solution.hs
@@ -1,7 +1,10 @@
 module Day1Solution where
 
-solve1 :: FilePath -> IO ()
-solve1 s = print =<< sum . map (reqFuel . read) . lines <$> readFile s
-    where
-        reqFuel :: Int -> Int
-        reqFuel n = n `div` 3 - 2
+solve :: FilePath -> IO ()
+solve fp = print =<< sum . map (reqFuel . read) . lines <$> readFile fp
+
+reqFuel :: Int -> Int
+reqFuel n = let fuel = n `div` 3 - 2 in
+    if fuel < 0
+        then 0
+        else fuel + reqFuel fuel
