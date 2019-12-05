d914b0fb67ecb172347ac3618b7614e326886c4c
Thomas Li <guslithd@student.gu.se>
2019-12-05 22:06:58 +0100

    Function for parsing instruction

--- solutions/Day5Solution.hs
+++ solutions/Day5Solution.hs
@@ -1,15 +1,61 @@
 module Day5Solution where
 
+import Data.Char (digitToInt)
 import Data.List.Split (splitOn)
 import qualified Data.Map.Strict as M
+    ( Map
+    , elems
+    , fromList
+    , insert
+    , lookup
+    , (!)
+    )
 
 type Mode = Int  -- 0 = Position mode, 1 = Immediate mode
 type Param = Int
 type Opcode = Int
 
 data Instruction = Instruction Opcode [(Mode, Param)]
+    deriving (Eq, Show)
 
-readInstruction :: [Int] -> (Instruction, Int)
+-- | Given an index, return the instruction and how many steps to move
+--   afterwards.
+parseInstruction :: Int -> M.Map Int Int -> (Instruction, Int)
+parseInstruction index m =
+    if length modes /= length params
+        then error "Non-matching length of modes and params!"
+        else (Instruction opcode (zip modes params), index + numParams + 1)
+
+    where
+        instructionBase = m M.! index
+        opcodeLength = 2
+
+        opcode :: Opcode
+        opcode = read
+               . reverse
+               . take opcodeLength
+               . reverse 
+               . show
+               $ instructionBase
+
+        modes :: [Mode]
+        modes = map digitToInt
+              . (\xs -> xs ++ replicate (numParams - length xs) '0')
+              . drop opcodeLength
+              . reverse 
+              . show
+              $ instructionBase
+
+        params :: [Param]
+        params = map (m M.!) [index + 1 .. index + numParams]
+
+        numParams :: Int
+        numParams = case opcode of
+          99 -> 0
+          1 -> 3
+          2 -> 3
+          3 -> 1
+          4 -> 1
 
 solve5 :: FilePath -> IO ()
 solve5 fp = do
