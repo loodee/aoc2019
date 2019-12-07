9eab3b26f0d56e3932eeac8dd81c957f547e2689
Thomas Li <guslithd@student.gu.se>
2019-12-07 06:23:34 +0100

    Updated Day 4 sweaty solution

--- solutions/Day4Sweaty.hs
+++ solutions/Day4Sweaty.hs
@@ -1,18 +1,15 @@
 module Day4Sweaty where
 
-import Data.List (group)
+import Data.List (group, sort)
 import Data.List.Split (splitOn)
 
 solve4sweaty :: FilePath -> IO ()
 solve4sweaty fp
     = print
     . length
-    . filter (\x -> ascends x && hasAdjacent x)
+    . filter (\xs -> sort xs == xs && (any ((2 ==) . length) . group) xs)
     . map show
     . (\(x, y) -> [x .. y] :: [Int])
     . (\xs -> (read . head $ xs, read . head . tail $ xs))
     . splitOn "-"
     =<< readFile fp
-    where hasAdjacent = any ((2 ==) . length) . group
-          ascends [x]    = True
-          ascends (x:xs) = x <= head xs && ascends xs
