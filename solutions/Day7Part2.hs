df97c803c50a4193e97715954e00096d9dd8e56e
Thomas Li <guslithd@student.gu.se>
2019-12-07 10:50:47 +0100

    Day 7 part 2 solution

--- solutions/Day7Part2.hs
+++ solutions/Day7Part2.hs
@@ -3,35 +3,44 @@ module Day7Part2 where
 import Data.List (permutations)
 
 import Day7Part2IntComputer
-    ( St (St, stOutputs)
+    ( St (St, stOutputs, stInputs)
     , parse
     , resumeExecution
-    , runWithArgs)
+    , runWithArgs
+    )
 
 type Tape = [Int]
 
 solve7p1 :: FilePath -> IO ()
-solve7p1 fp = do
-    tape <- parse <$> readFile fp
+solve7p1 fp = print
+            =<< (\t -> maxThrusterSignal t $ permutations [5 .. 9])
+            . parse
+            =<< readFile fp
 
-    return ()
+maxThrusterSignal :: Tape     -- Program tape.
+                  -> [[Int]]  -- All combinations of possible phase settings.
+                  -> IO Int   -- The highest output signal to the thrusters.
+maxThrusterSignal _ [] = return 0
+maxThrusterSignal tape (ps : pss) = do
+    sts <- startAmplification tape ps [0]
+    signal <- runAmplification (map fst sts) (snd $ last sts)
+    max signal <$> maxThrusterSignal tape pss
 
-numAmps :: Int
-numAmps = 5
-
-
+-- ^-This-^ and v-this-v use ugly hacks to get correct inputs,
+-- should be fixed at some point.
 
 -- | Puts the feedback loop into action for one combination
 --   of phase settings.
-startAmplificationH :: Tape     -- Program tape.
-                    -> [Int]    -- One combination of phase settings.
-                    -> [Int]    -- Initial inputs.
-                    -> IO [St]  -- Running amplifiers.
-startAmplificationH _ [] _ = return []
-startAmplificationH tape (phaseSetting : ps) inputs = do
+startAmplification :: Tape              -- Program tape.
+                   -> [Int]             -- One combination of phase settings.
+                   -> [Int]             -- Initial inputs.
+                   -> IO [(St, [Int])]  -- Running amplifiers and each of
+                                        -- their outputs (first halt).
+startAmplification _ [] _ = return []
+startAmplification tape (phaseSetting : ps) inputs = do
     postSt@(St _ outputs _ _ _) <- runWithArgs tape (phaseSetting : inputs)
-    xs <- startAmplificationH tape ps (reverse outputs)
-    return $ postSt { stOutputs = [] } : xs
+    xs <- startAmplification tape ps (reverse outputs)
+    return $ (postSt { stOutputs = [] }, reverse outputs) : xs
 
 -- | Loop an amplification run for one combination of phase settings
 --   and return the output signal after halting.
