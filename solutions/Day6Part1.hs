68b87161be8d1291187aff156586df109ea58f04
Thomas Li <guslithd@student.gu.se>
2019-12-10 03:18:29 +0100

    D6: Part 1 solution

--- solutions/Day6Part1.hs
+++ solutions/Day6Part1.hs
@@ -1,9 +1,26 @@
 module Day6Part1 where
 
-data Planet = Planet String [Planet]
-    deriving (Eq, Show)
+import Data.List (partition)
+import Data.List.Split (splitOn)
+
+import Data.Tree
 
 type Orbit = (String, String)
 
 solve6p1 :: FilePath -> IO ()
-solve6p1 = undefined
+solve6p1 fp = print . count 0 . (`build` com) . parse =<< readFile fp
+
+com :: Tree String
+com = Node "COM" []
+
+parse :: String -> [Orbit]
+parse = map ((\(x:y:_) -> (x, y)) . splitOn ")") . lines
+
+build :: [Orbit] -> Tree String -> Tree String
+build rem (Node name xs) =
+  let (orbits, rest) = partition ((name ==) . fst) rem
+  in Node name $ map (build rest . (\x -> Node x []) . snd) orbits
+
+count :: Int -> Tree String -> Int
+count d (Node _ []) = d
+count d (Node n xs) = d + sum (map (count $ d + 1) xs)
