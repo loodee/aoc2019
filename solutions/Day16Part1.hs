9c4a9f4d076790481c7a7b2b850700b88ca410c5
Thomas Li <guslithd@student.gu.se>
2019-12-17 01:38:18 +0100

    D16: Part 1 solution

---
+++ solutions/Day16Part1.hs
@@ -0,0 +1,23 @@
+module Day16Part1 where
+
+import Data.Char (digitToInt, intToDigit)
+
+solve16p1IO :: FilePath -> IO ()
+solve16p1IO fp = readFile fp >>= (putStrLn . solve16p1)
+
+solve16p1 :: String -> String
+solve16p1
+  = map intToDigit
+  . take 8
+  . (\x -> iterate fft x !! 100)
+  . map digitToInt
+
+fft :: [Int] -> [Int]
+fft xs = take (length xs) $ map (calcDigit xs) [1 .. ]
+  where
+    pat n = tail . cycle . concatMap (replicate n) $ [0, 1, 0, -1]
+
+    calcDigit :: [Int] -> Int -> Int
+    calcDigit input n = (% 10) . abs . sum . zipWith (*) input $ pat n
+      where
+        a % b = if b /= 0 then a `mod` b else 0
